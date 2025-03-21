3f9b056b4798bc203e0be9b0db8bba8d0af14695
dborziplank <dborzi@plank.global>
2025-03-21 13:57:48 +0000

    Ticket 324349 - pagina in coda eems

---
+++ updates/20250321.sql
@@ -0,0 +1,2 @@
+INSERT INTO `comm`.`plank_parametri` (`id_par`, `particellaazienda`, `valore`) VALUES ('140', 'eems', 'D:\\\\/xampp\\/htdocs\\/plank\\/producipdf_template\\/eems\\/A4_fattura_def.pdf');
+INSERT INTO `comm`.`plank_parametri` (`id_par`, `particellaazienda`, `valore`) VALUES ('141', 'eems', 'D:\\\\/xampp\\/htdocs\\/plank\\/producipdf_template\\/eems\\/A4_fattura_def.pdf');
