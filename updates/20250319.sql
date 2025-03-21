dc094afeb2d1f04ad6d7c800b04c14247c53005f
dborziplank <dborzi@plank.global>
2025-03-21 10:53:53 +0000

    traduzioni attiva online in json object

--- updates/20250319.sql
+++ updates/20250319.sql
@@ -1,54 +1,39 @@
+UPDATE `comm`.`plank_contrattidigitali_tipoconnessione` SET `label` = '{\"italian\":\"Permanente\",\"english\":\"Permanent\"}' WHERE (`id` = '1');
+UPDATE `comm`.`plank_contrattidigitali_tipoconnessione` SET `label` = '{\"italian\":\"Temporanea\",\"english\":\"Temporary\"}' WHERE (`id` = '2');
+UPDATE `comm`.`plank_contrattidigitali_tipoconnessione` SET `label` = '{\"italian\":\"Costruzione in muratura\",\"english\":\"Masonry construction\"}' WHERE (`id` = '3');
+UPDATE `comm`.`plank_contrattidigitali_tipoconnessione` SET `label` = '{\"italian\":\"Costruzione mobile o temporanea\",\"english\":\"Mobile or temporary construction\"}' WHERE (`id` = '4');
 ALTER TABLE `comm`.`plank_contrattidigitali_tipoconnessione` 
-ADD COLUMN `lang` VARCHAR(45) NULL AFTER `particellaazienda`;
+CHANGE COLUMN `label` `label` JSON NULL DEFAULT NULL;
 
-ALTER TABLE `comm`.`plank_contrattidigitali_tipoconnessione` 
-DROP INDEX `particella_tipo_unique` ,
-ADD UNIQUE INDEX `particella_tipo_lang_unique` (`particellaazienda` ASC, `tipoconnessione` ASC, `lang` ASC) VISIBLE;
-
-INSERT INTO `comm`.`plank_contrattidigitali_tipoconnessione` (`tipoconnessione`, `label`, `lang`) VALUES ('PT', 'Permanent', 'english');
-INSERT INTO `comm`.`plank_contrattidigitali_tipoconnessione` (`tipoconnessione`, `label`, `lang`) VALUES ('TP', 'Temporary', 'english');
-INSERT INTO `comm`.`plank_contrattidigitali_tipoconnessione` (`tipoconnessione`, `label`, `particellaazienda`, `lang`) VALUES ('PT', 'Masonry construction', 'igl', 'english');
-INSERT INTO `comm`.`plank_contrattidigitali_tipoconnessione` (`tipoconnessione`, `label`, `particellaazienda`, `lang`) VALUES ('TP', 'Mobile or temporary construction', 'igl', 'english');
-UPDATE `comm`.`plank_contrattidigitali_tipoconnessione` SET `lang` = 'italian' WHERE (`id` = '1');
-UPDATE `comm`.`plank_contrattidigitali_tipoconnessione` SET `lang` = 'italian' WHERE (`id` = '2');
-UPDATE `comm`.`plank_contrattidigitali_tipoconnessione` SET `lang` = 'italian' WHERE (`id` = '3');
-UPDATE `comm`.`plank_contrattidigitali_tipoconnessione` SET `lang` = 'italian' WHERE (`id` = '4');
-
+UPDATE `comm`.`plank_contrattidigitali_usoenergia` SET `label` = '{\"english\": \"Residence\",\"italian\": \"Abitazione\"}' WHERE (`id` = '1');
+UPDATE `comm`.`plank_contrattidigitali_usoenergia` SET `label` = '{\"english\": \"Commercial activity\",\"italian\": \"Attività commerciale\"}' WHERE (`id` = '2');
+UPDATE `comm`.`plank_contrattidigitali_usoenergia` SET `label` = '{\"english\": \"Other residential uses (e.g., garage; stairwell lighting)\",\"italian\": \"Altri usi abitativi (esempio: garage; luce scala)\"}' WHERE (`id` = '3');
+UPDATE `comm`.`plank_contrattidigitali_usoenergia` SET `label` = '{\"english\": \"Other non-commercial uses (e.g., storage; electricity for non-commercial agricultural use; vineyard hoist)\",\"italian\": \"Altri usi non commerciali (esempio: Deposito; corrente per uso agricoltura non commerciale; montacarichi della vigna)\"}' WHERE (`id` = '4');
+UPDATE `comm`.`plank_contrattidigitali_usoenergia` SET `label` = '{\"english\": \"Temporary meter (e.g., construction site; temporary lighting)\",\"italian\": \"Contatore temporaneo (esempio: cantiere edile; illuminazione temporanea)\"}' WHERE (`id` = '5');
+UPDATE `comm`.`plank_contrattidigitali_usoenergia` SET `label` = '{\"english\": \"Event (e.g., festivals or fairs)\",\"italian\": \"Manifestazione (esempio: sagre o fiere)\"}' WHERE (`id` = '6');
+UPDATE `comm`.`plank_contrattidigitali_usoenergia` SET `label` = '{\"english\": \"Other\",\"italian\": \"Altro\"}' WHERE (`id` = '7');
+UPDATE `comm`.`plank_contrattidigitali_usoenergia` SET `label_sottotipo_uso` = '{\"english\": \"A building constructed after January 30, 1977\",\"italian\": \"Un immobile costruito dopo il 30 gennaio 1977\"}' WHERE (`id` = '8');
+UPDATE `comm`.`plank_contrattidigitali_usoenergia` SET `label_sottotipo_uso` = '{\"english\": \"A building constructed before January 30, 1977\",\"italian\": \"Un immobile costruito prima del 30 gennaio 1977\"}' WHERE (`id` = '9');
+UPDATE `comm`.`plank_contrattidigitali_usoenergia` SET `label_sottotipo_uso` = '{\"english\": \"Modify the building to make it compliant with the registered municipal project\",\"italian\": \"Modificare l’immobile rendendolo conforme al progetto registrato in comune\"}' WHERE (`id` = '10');
+UPDATE `comm`.`plank_contrattidigitali_usoenergia` SET `label_sottotipo_uso` = '{\"english\": \"Modify a pre-existing building\",\"italian\": \"Modificare un immobile preesistente\"}' WHERE (`id` = '11');
+UPDATE `comm`.`plank_contrattidigitali_usoenergia` SET `label_sottotipo_uso` = '{\"english\": \"Fair or Event for which you have authorization to occupy public land\",\"italian\": \"Fiera o Manifestazione per cui hai autorizzazione di occupazione del suolo pubblico\"}' WHERE (`id` = '12');
+UPDATE `comm`.`plank_contrattidigitali_usoenergia` SET `label_sottotipo_uso` = '{\"english\": \"Water pumps\",\"italian\": \"Pompe per l’acqua\"}' WHERE (`id` = '13');
+UPDATE `comm`.`plank_contrattidigitali_usoenergia` SET `label_sottotipo_uso` = '{\"english\": \"Temporary installation for urban planning purposes\",\"italian\": \"L’installazione temporanea a fine urbanistico\"}' WHERE (`id` = '14');
+UPDATE `comm`.`plank_contrattidigitali_usoenergia` SET `label_sottotipo_uso` = '{\"english\": \"A place without buildings, facilities, or works\",\"italian\": \"Un luogo senza immobili, impianti o opere\"}' WHERE (`id` = '15');
+UPDATE `comm`.`plank_contrattidigitali_usoenergia` SET `label` = '{\"english\": \"Ordinary [Permanent]\",\"italian\": \"Ordinaria [Permanente]\"}' WHERE (`id` = '16');
+UPDATE `comm`.`plank_contrattidigitali_usoenergia` SET `label` = '{\"english\": \"Piecework [Permanent]\",\"italian\": \"Cottimo [Permanente]\"}' WHERE (`id` = '17');
+UPDATE `comm`.`plank_contrattidigitali_usoenergia` SET `label` = '{\"english\": \"Installation not permanently manned, located outside inhabited areas [Particular Permanent]\",\"italian\": \"Installazione non presidiata in permanenza, situata fuori dagli abitati [Permanente Particolare]\"}' WHERE (`id` = '18');
+UPDATE `comm`.`plank_contrattidigitali_usoenergia` SET `label` = '{\"english\": \"Luminous and advertising signs [Particular Permanent]\",\"italian\": \"Insegne luminose e pubblicitarie [Permanente Particolare]\"}' WHERE (`id` = '19');
+UPDATE `comm`.`plank_contrattidigitali_usoenergia` SET `label` = '{\"english\": \"Monument illumination system and similar [Particular Permanent]\",\"italian\": \"Impianto di illuminazione di monumenti e simili [Permanente Particolare]\"}' WHERE (`id` = '20');
+UPDATE `comm`.`plank_contrattidigitali_usoenergia` SET `label` = '{\"english\": \"Lift system and similar [Particular Permanent]\",\"italian\": \"Impianto di risalita e simili [Permanente Particolare]\"}' WHERE (`id` = '21');
+UPDATE `comm`.`plank_contrattidigitali_usoenergia` SET `label` = '{\"english\": \"Mobile and precarious installations [Particular Permanent]\",\"italian\": \"Installazione mobili e precarie [Permanente Particolare]\"}' WHERE (`id` = '22');
+UPDATE `comm`.`plank_contrattidigitali_usoenergia` SET `label` = '{\"english\": \"Single construction not permanently inhabited or not the official residence of the owner (villas, hunting lodges, mountain shelters, etc.) located more than 2,000 meters from the reference medium/low voltage substation [Particular Permanent]\",\"italian\": \" Singola costruzione non abitata in permanenza o comunque non di  residenza anagrafica del proprietario (ville, case di caccia, rifugi di montagna e simili) situata oltre 2.000 metri dalla cabina media/bassa tensione di riferimento [Permanente Particolare]\"}' WHERE (`id` = '23');
+UPDATE `comm`.`plank_contrattidigitali_usoenergia` SET `label` = '{\"english\": \"Construction not accessible by motorable roads or separated from existing distribution networks by stretches of sea, lake, or lagoon [Particular Permanent]\",\"italian\": \" Costruzione che non sia raggiungibile con strada percorribile da automezzi o che sia separata dagli impianti di distribuzione esistenti da tratti di mare, di lago o laguna [Permanente Particolare]\"}' WHERE (`id` = '24');
+UPDATE `comm`.`plank_contrattidigitali_usoenergia` SET `label` = '{\"english\": \"Construction site [Temporary]\",\"italian\": \"Cantiene [Temporanea]\"}' WHERE (`id` = '25');
+UPDATE `comm`.`plank_contrattidigitali_usoenergia` SET `label` = '{\"english\": \"Traveling show and similar [Extraordinary Temporary]\",\"italian\": \"Spettacolo viaggiante e simili [Temporanea Straordinaria]\"}' WHERE (`id` = '26');
+UPDATE `comm`.`plank_contrattidigitali_usoenergia` SET `label` = '{\"english\": \"Installation made for events and patronal, political, religious, sports, theatrical festivities, and similar [Extraordinary Temporary]\",\"italian\": \"Impianto realizzato in occasioni di manifestazioni e feste patronali, politiche, religiose, sportive, teatrali e simili [Temporanea Straordinaria]\"}' WHERE (`id` = '27');
+UPDATE `comm`.`plank_contrattidigitali_usoenergia` SET `label` = '{\"english\": \"Installation for cinematographic, television, and similar recordings [Extraordinary Temporary]\",\"italian\": \"Impianto relativo a riprese cinematogragiche, televisive e simili [Temporanea Straordinaria]\"}' WHERE (`id` = '28');
+UPDATE `comm`.`plank_contrattidigitali_usoenergia` SET `label` = '{\"english\": \"Temporary supply other than construction site with non-subsidized contribution [Extraordinary Temporary]\",\"italian\": \"Fornitura Temporanea diversa da cantiere concontributo non agevolato [Temporanea Straordinaria]\"}' WHERE (`id` = '29');
 ALTER TABLE `comm`.`plank_contrattidigitali_usoenergia` 
-ADD COLUMN `lang` VARCHAR(45) NULL AFTER `particellaazienda`;
-
-INSERT INTO `comm`.`plank_contrattidigitali_usoenergia` (`uso_energia`, `label`, `particellaazienda`, `lang`) VALUES 
-('001', 'Residence', 'IGL', 'english'),
-('002', 'Commercial activity', 'IGL', 'english'),
-('003', 'Other residential uses (e.g., garage; stairwell lighting)', 'IGL', 'english'),
-('004', 'Other non-commercial uses (e.g., storage; electricity for non-commercial agricultural use; vineyard hoist)', 'IGL', 'english'),
-('010', 'Temporary meter (e.g., construction site; temporary lighting)', 'IGL', 'english'),
-('012', 'Event (e.g., festivals or fairs)', 'IGL', 'english'),
-('014', 'Other', 'IGL', 'english');
-INSERT INTO `comm`.`plank_contrattidigitali_usoenergia` (`uso_energia`, `sottotipo_uso`, `label_sottotipo_uso`, `particellaazienda`, `lang`) VALUES 
-('014', '014_1', 'A building constructed after January 30, 1977', 'IGL', 'english'),
-('014', '014_2', 'A building constructed before January 30, 1977', 'IGL', 'english'),
-('014', '014_3', 'Modify the building to make it compliant with the registered municipal project', 'IGL', 'english'),
-('014', '014_4', 'Modify a pre-existing building', 'IGL'),
-('014', '014_5', 'Fair or Event for which you have authorization to occupy public land', 'IGL', 'english'),
-('014', '014_6', 'Water pumps', 'IGL'),
-('014', '014_7', 'Temporary installation for urban planning purposes', 'IGL', 'english'),
-('014', '014_8', 'A place without buildings, facilities, or works', 'IGL', 'english');
-INSERT INTO `comm`.`plank_contrattidigitali_usoenergia` (`uso_energia`, `label`, `lang`) VALUES 
-('001', 'Ordinary [Permanent]'),('002', 'Piecework [Permanent]', 'english'),
-('003', 'Installation not permanently manned, located outside inhabited areas [Particular Permanent]', 'english'),
-('004', 'Luminous and advertising signs [Particular Permanent]', 'english'),
-('005', 'Monument illumination system and similar [Particular Permanent]', 'english'),
-('006', 'Lift system and similar [Particular Permanent]'),
-('007', 'Mobile and precarious installations [Particular Permanent]', 'english'),
-('008', 'Single construction not permanently inhabited or not the official residence of the owner (villas, hunting lodges, mountain shelters, etc.) located more than 2,000 meters from the reference medium/low voltage substation [Particular Permanent]', 'english'),
-('009', 'Construction not accessible by motorable roads or separated from existing distribution networks by stretches of sea, lake, or lagoon [Particular Permanent]', 'english'),
-('010', 'Construction site [Temporary]'),
-('011', 'Traveling show and similar [Extraordinary Temporary]'),
-('012', 'Installation made for events and patronal, political, religious, sports, theatrical festivities, and similar [Extraordinary Temporary]', 'english'),
-('013', 'Installation for cinematographic, television, and similar recordings [Extraordinary Temporary]', 'english'),
-('015', 'Temporary supply other than construction site with non-subsidized contribution [Extraordinary Temporary]', 'english');
-
-UPDATE `comm`.`plank_contrattidigitali_usoenergia` SET `lang` = 'italian' WHERE (`id` IN ('1', '2'
-'3', '4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22',
-'23','24','25','26','27','28','29'));
+CHANGE COLUMN `label` `label` JSON NULL DEFAULT NULL ,
+CHANGE COLUMN `label_sottotipo_uso` `label_sottotipo_uso` JSON NULL DEFAULT NULL ;
